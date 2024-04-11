172d2910d65dca3cdb52c456ff1a3f8e8488fbba
Noah Croghan <noahcroghan0@gmail.com>
2024-04-11 14:26:34 -0500

    First Changes
    Nonfunctional

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,63 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import styles from "./App.module.css";
 import * as Tone from "tone";
 
-function App() {
-  const [count, setCount] = useState(0);
+const NUM_STEPS = 16;
+
+export default function App() {
+  const [sequence, setSequence] = useState(new Array(NUM_STEPS).fill(false));
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const synth = new Tone.Synth().toDestination();
+
+  useEffect(() => {
+    Tone.Transport.bpm.value = 120;
+  }, []);
+
+  const toggleStep = (index) => {
+    setSequence((prevSequence) => {
+      const newSequence = [...prevSequence];
+      newSequence[index] = !newSequence[index];
+      return newSequence;
+    });
+  };
+
+  const playStop = () => {
+    setIsPlaying(!isPlaying);
+    if (isPlaying) {
+      Tone.Transport.stop();
+    } else {
+      Tone.Transport.start();
+    }
+  };
+
+  const loop = (time) => {
+    const step = Tone.Transport.position % NUM_STEPS;
+    if (sequence[step]) {
+      synth.triggerAttackRelease("C4", "8n", time);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="sequencer">
+        <button onClick={playStop}>
+          {isPlaying ? (
+            <img src="./assets/pause.svg" alt="Pause Icon" />
+          ) : (
+            <img src="./assets/play.svg" alt="Play Icon" />
+          )}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="steps">
+          {sequence.map((isActive, index) => (
+            <button
+              key={index}
+              className={`step ${isActive ? "active" : ""}`}
+              onClick={() => toggleStep(index)}
+            />
+          ))}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
-
-export default App;
